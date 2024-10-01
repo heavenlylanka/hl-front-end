f15583befd2727fe9175c02fff52426045a6056d
Dilukshan <dilukshan2020da@gmail.com>
2024-10-01 09:30:26 +0530

    change-submitForm
    Change alignment/responsive

--- src/components/SubmitForm.tsx
+++ src/components/SubmitForm.tsx
@@ -15,8 +15,8 @@ const QwigleyFont = Qwigley({
 
 function SubmitForm() {
   return (
-    <div className="container mx-auto px-4 py-16 flex flex-row items-center">
-      <div className="text-center max-w-3xl mb-12">
+    <div className="container mx-auto px-4 py-16 flex flex-col lg:flex-row items-center justify-center bg-gray-200">
+      <div className="text-center lg:text-start max-w-3xl mb-12 bg-green-100 lg:max-w-98">
         <h1
           className={`${QwigleyFont.className} text-[180px] leading-none mb-4 
         bg-gradient-to-r from-pink-500 via-yellow-500 to-yellow-300 text-transparent bg-clip-text`}
@@ -24,7 +24,7 @@ function SubmitForm() {
           Sri Lanka
         </h1>
 
-        <p className="text-lg text-black">
+        <p className="text-lg lg:pr-20 text-black lg">
           A land where history whispers from ancient ruins, where nature paints
           with vibrant hues, and where warm smiles greet you at every turn.
           Embark on a journey through our island paradise and uncover
@@ -33,7 +33,7 @@ function SubmitForm() {
       </div>
 
       {/* right form */}
-      <div className="relative w-[300px] h-[500px]">
+      <div className="relative w-80 lg:w-auto h-auto items-center bg-blue-300">
         <form className="relative z-10 w-full lg:w-[400px] flex flex-col justify-center pt-6 lg:pt-8 bg-black bg-opacity-50 p-6 rounded-lg">
             
           <div className="flex flex-col gap-1 pt-4">
@@ -67,7 +67,7 @@ function SubmitForm() {
 
           <Button2
             variant="secondary"
-            className="w-[100px] bg-[#59AE5B] text-white flex items-center"
+            className="w-[100px] bg-[#59AE5B] text-white flex items-center mt-5"
           >
             Submit <Icons.doubleArrows className="h-6 w-6" />
           </Button2> 

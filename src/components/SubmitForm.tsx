91107366fa3a836a31c0162553956ebe0893c23f
Dilukshan <dilukshan2020da@gmail.com>
2024-10-01 17:45:22 +0530

    change submit form
    change background, alignment, image

--- src/components/SubmitForm.tsx
+++ src/components/SubmitForm.tsx
@@ -15,8 +15,8 @@ const QwigleyFont = Qwigley({
 
 function SubmitForm() {
   return (
-    <div className="container mx-auto px-4 py-16 flex flex-col lg:flex-row items-center justify-center bg-gray-200">
-      <div className="text-center lg:text-start max-w-3xl mb-12 bg-green-100 lg:max-w-98">
+    <div className="container mx-auto px-4 py-16 flex flex-col lg:flex-row items-center justify-center">
+      <div className="text-center lg:text-start max-w-3xl mb-12 lg:max-w-98">
         <h1
           className={`${QwigleyFont.className} text-[180px] leading-none mb-4 
         bg-gradient-to-r from-pink-500 via-yellow-500 to-yellow-300 text-transparent bg-clip-text`}
@@ -24,7 +24,7 @@ function SubmitForm() {
           Sri Lanka
         </h1>
 
-        <p className="text-lg lg:pr-20 text-black lg">
+        <p className="text-lg mx-4 lg:pr-20 text-black lg">
           A land where history whispers from ancient ruins, where nature paints
           with vibrant hues, and where warm smiles greet you at every turn.
           Embark on a journey through our island paradise and uncover
@@ -33,8 +33,9 @@ function SubmitForm() {
       </div>
 
       {/* right form */}
-      <div className="relative w-80 lg:w-auto h-auto items-center bg-blue-300">
-        <form className="relative z-10 w-full lg:w-[400px] flex flex-col justify-center pt-6 lg:pt-8 bg-black bg-opacity-50 p-6 rounded-lg">
+      <div className="relative w-96 lg:w-auto h-auto items-center py-12 px-2 bg-cover  flex justify-center  bg-[url('/assets/submitform_bg.png')] rounded-3xl">
+        <div className="absolute inset-0 bg-black bg-opacity-50 rounded-3xl"></div>
+        <form className="relative z-10 w-full lg:w-[400px] flex flex-col justify-center pt-6 lg:pt-8 p-6">
             
           <div className="flex flex-col gap-1 pt-4">
             <label className="text-[16px] font-medium text-white">Name</label>
@@ -72,6 +73,7 @@ function SubmitForm() {
             Submit <Icons.doubleArrows className="h-6 w-6" />
           </Button2> 
         </form>
+        
       </div>
     </div>
   );

51de81c22be7c9d435da6d8074cc48e4015f44a6
Dilukshan <dilukshan2020da@gmail.com>
2024-10-02 20:35:56 +0530

    change navbar
    increase z index

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,129 +1,109 @@
-"use client";
-import React, { useState } from "react";
+"use client"; // Add this line at the top
+import React, { useState } from 'react';
 import { IoCallOutline } from "react-icons/io5";
-import Link from "next/link";
+import Link from 'next/link';
 
 const Navbar = () => {
-  const [isOpen, setIsOpen] = useState(false);
+    const [isOpen, setIsOpen] = useState(false);
 
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
-  };
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
 
-  return (
-    <nav className="bg-white border-gray-200 dark:bg-gray-900 w-full fixed top-0 left-0 z-10">
-      <div className="w-full flex flex-wrap items-center justify-between mx-auto p-4">
-        <a className="flex items-center space-x-3 rtl:space-x-reverse" href="/">
-          <img src="Logo.svg" className="h-8" alt="Hevanly Logo" />
-        </a>
-        <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-          <button
-            type="button"
-            className="flex items-center text-white bg-orange-400 hover:bg-orange-500 focus:ring-4 
-            focus:outline-none focus:ring-orange-300 font-medium rounded-lg text-sm px-4 py-2 text-center
-             dark:bg-orange-600 dark:hover:bg-orange-700 dark:focus:ring-orange-800"
-          >
-            Call Us
-            <IoCallOutline className="ml-2" />
-          </button>
-          <button
-            onClick={toggleMenu}
-            data-collapse-toggle="navbar-cta"
-            type="button"
-            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg 
-            md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400
-             dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-            aria-controls="navbar-cta"
-            aria-expanded={isOpen}
-          >
-            <span className="sr-only">Open main menu</span>
-            <svg
-              className="w-5 h-5"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 17 14"
-            >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M1 1h15M1 7h15M1 13h15"
-              />
-            </svg>
-          </button>
-        </div>
-        <div
-          className={`${
-            isOpen ? "block" : "hidden"
-          } items-center justify-between w-full md:flex md:w-auto md:order-1`}
-          id="navbar-cta"
-        >
-          <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg
-           bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white
-            dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-            <li>
-              <Link href="/" legacyBehavior>
-                <a
-                  className="block py-2 px-3 md:p-0 text-white bg-blue-700 rounded md:bg-transparent
-                   md:text-blue-700 md:dark:text-blue-500"
-                  aria-current="page"
-                >
-                  Home
-                </a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/tours" legacyBehavior>
-                <a className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent
-                 md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700
-                  dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
-                  Our Tours
-                </a>
-              </Link>
-            </li>
-            <li>
-              <Link href="#" legacyBehavior>
-                <a className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent
-                 md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700
-                  dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
-                  Destinations
-                </a>
-              </Link>
-            </li>
-            <li>
-              <Link href="#" legacyBehavior>
-                <a className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent
-                 md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700
-                  dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
-                  Things To Know
-                </a>
-              </Link>
-            </li>
-            <li>
-              <Link href="#" legacyBehavior>
-                <a className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent
-                 md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700
-                  dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
-                  About Us
-                </a>
-              </Link>
-            </li>
-            <li>
-              <Link href="#" legacyBehavior>
-                <a className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent
-                 md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700
-                  dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
-                  Contact Us
-                </a>
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-  );
+    return (
+        <nav className="bg-white border-gray-200 dark:bg-gray-900 w-full fixed top-0 left-0 z-50">
+            <div className="w-full flex flex-wrap items-center justify-between mx-auto p-4">
+                <Link href="https://flowbite.com/" legacyBehavior>
+                    <a className="flex items-center space-x-3 rtl:space-x-reverse">
+                        <img src="Logo.svg" className="h-8" alt="Hevanly Logo" />
+                        
+                    </a>
+                </Link>
+                <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+                <button
+                  type="button"
+                  className="flex items-center text-white bg-orange-400 hover:bg-orange-500 focus:ring-4 focus:outline-none focus:ring-orange-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-orange-600 dark:hover:bg-orange-700 dark:focus:ring-orange-800">
+                  Call Us
+                  <IoCallOutline className="ml-2" />
+              </button>
+                    <button
+                        onClick={toggleMenu}
+                        data-collapse-toggle="navbar-cta"
+                        type="button"
+                        className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                        aria-controls="navbar-cta"
+                        aria-expanded={isOpen}
+                    >
+                        <span className="sr-only">Open main menu</span>
+                        <svg
+                            className="w-5 h-5"
+                            aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 17 14"
+                        >
+                            <path
+                                stroke="currentColor"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M1 1h15M1 7h15M1 13h15"
+                            />
+                        </svg>
+                    </button>
+                </div>
+                <div className={`${isOpen ? 'block' : 'hidden'} items-center justify-between w-full md:flex md:w-auto md:order-1`} id="navbar-cta">
+                    <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                        <li>
+                            <Link href="/" legacyBehavior>
+                                <a
+                                    className="block py-2 px-3 md:p-0 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:dark:text-blue-500"
+                                    aria-current="page"
+                                >
+                                    Home
+                                </a>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="#" legacyBehavior>
+                                <a className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+                                    Our Tours
+                                </a>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="#" legacyBehavior>
+                                <a className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+                                    Destinations
+                                </a>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="#" legacyBehavior>
+                                <a className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+                                    Things To Know
+                                </a>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="#" legacyBehavior>
+                                <a className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+                                    About Us
+                                </a>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="#" legacyBehavior>
+                                <a className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+                                    Contact Us
+                                </a>
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
 };
 
 export default Navbar;

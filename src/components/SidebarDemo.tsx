87ba928d3af0ef9a162d42740c48d2b5978c0602
Uditha Nuwan <udithanuwanbandara2@gmail.com>
2024-10-02 17:25:53 -0700

    update user profile page after connecting backend

--- src/components/SidebarDemo.tsx
+++ src/components/SidebarDemo.tsx
@@ -64,23 +64,7 @@ export function SidebarDemo({children}:Readonly<{children:React.ReactNode}>) {
               ))}
             </div>
           </div>
-          <div>
-            <SidebarLink
-              link={{
-                label: "Manu Arora",
-                href: "#",
-                icon: (
-                  <Image
-                    src="https://assets.aceternity.com/manu.png"
-                    className="h-7 w-7 flex-shrink-0 rounded-full"
-                    width={50}
-                    height={50}
-                    alt="Avatar"
-                  />
-                ),
-              }}
-            />
-          </div>
+          
         </SidebarBody>
       </Sidebar>
       {children}

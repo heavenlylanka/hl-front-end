dda1faf5cd5579ddb037d51d7c253efaf6b7bdde
Dilukshan <dilukshan2020da@gmail.com>
2024-10-27 11:01:57 +0530

    BookingHistory
    Frontend for BookingHistory- yourtrips.tsx /BookingList.tsx

--- src/components/SidebarDemo.tsx
+++ src/components/SidebarDemo.tsx
@@ -31,7 +31,7 @@ export function SidebarDemo({children}:Readonly<{children:React.ReactNode}>) {
     },
     {
       label: "Your Trips",
-      href: "/yourtips",
+      href: "/yourtrips",
       icon: (
         <IconArrowLeft className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
       ),

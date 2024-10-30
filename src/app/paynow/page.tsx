135ad91907a9efe4b6ae1bf87d04767a58aa0927
Dilukshan <dilukshan2020da@gmail.com>
2024-10-30 10:15:41 +0530

    create paynow
    test commit

---
+++ src/app/paynow/page.tsx
@@ -0,0 +1,15 @@
+import Navbar from '@/components/Navbar'
+import React from 'react'
+
+const Paynow = () => {
+  return (
+    <div>
+        <Navbar/>
+        <div className='mt-20'>
+            <h1>You have to make 10% advance of full payment for booking</h1>
+        </div>
+    </div>
+  )
+}
+
+export default Paynow
